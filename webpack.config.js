2bc2d1e2cba2909fcbcc11d85158a3daa7bfd05a
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-08-27 18:00:06 +0100

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: './src/index/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new htmlWebpackPlugin({
+        title: 'Todo List',
+        template: './src/index/index.html',
+        filename: './index.html',
+    })
+  ]
 };
