84843b2642d243a5d0a94ca24877f2b493bb07ea
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-09-13 12:16:27 +0100

    Enable CSS modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import ViewController from "./view/ViewController";
 import ProjectsContainer from "./logic/ProjectsContainer";
 
-import normalizeCss from "./normalize.css";
-import css from "./index.css";
-
 ViewController.updateView();
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          }
+        ],
       },
     ],
   },
