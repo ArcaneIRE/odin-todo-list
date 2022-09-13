9ff7cfe5683f602ee5d67a950591260d6cf41197
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-09-13 12:17:57 +0100

    Change header css to module

--- src/view/Header.js
+++ src/view/Header.js
@@ -1,6 +1,8 @@
 import PubSub from "pubsub-js";
 import { CREATE_PROJECT } from "../event-types";
 
+import headerStyles from './Header.css';
+
 export function createHeader() {
     const headerMaker = new Header();
     headerMaker.addTitle();
@@ -12,19 +14,19 @@ export function createHeader() {
 class Header {
     constructor() {
         this.header = document.createElement('header');
-        this.header.classList = 'header';
+        this.header.classList = headerStyles['header'];
     }
 
     addTitle() {
         const title = document.createElement('h1');
-        title.classList = 'header-title';
+        title.classList = headerStyles['title'];
         title.innerText = 'Todo List.';
         this.header.appendChild(title);
     }
 
     addNewProjectButton() {
         const addProjectButton = document.createElement('button');
-        addProjectButton.classList = 'header-new-group-button';
+        addProjectButton.classList = headerStyles['new-group-button'];
         addProjectButton.innerText = 'New Project';
         addProjectButton.addEventListener('click', () => {
             PubSub.publish(CREATE_PROJECT);
